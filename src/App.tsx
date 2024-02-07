c85aec8270c0da8e1359e735831128d91dee247e
Zehra Şeren <fatmazehraseren@gmail.com>
2024-02-07 08:47:53 +0300

    Input Change Event

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import "./App.css";
 
 const App: FC = () => {
-  return <div className="App">    
-  </div>;
+  const [task, setTask] = useState<string>("");
+  const [workDay, setWorkDay] = useState<number>(0);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setWorkDay(Number(event.target.value));
+    }
+  };
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Taskı giriniz."
+        value={task}
+        onChange={handleChange}
+        name="task"
+      />
+      <input
+        type="number"
+        placeholder="Kaç günde tamamlamalısınız?"
+        value={workDay}
+        onChange={handleChange}
+        name="workDay"
+      />
+      <button>Yeni Task Ekle</button>
+    </div>
+  );
 };
 
 export default App;
